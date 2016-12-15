9d2b24ae4df249f70ace31c05712d54037a3788a
Stoumfou <alexis.pambourg@gmail.com>
2016-12-15 22:07:08 +0100

    REVERT SUR LES FRAGMENTS
    Bon trop galère les fragments. Je ferais si j'ai le temps mais on va
    créer d'autre activity

--- app/src/main/java/com/example/bouveti/geophone/MainActivity.java
+++ app/src/main/java/com/example/bouveti/geophone/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.bouveti.geophone;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,18 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import java.util.Locale;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 
@@ -159,9 +149,8 @@ public class MainActivity extends AppCompatActivity
 
     public void ToRecherche(View v)
     {
-        Intent intent = new Intent(this , MainActivity.class);
+        Intent intent = new Intent(this , RechercheActivity.class);
         startActivity(intent);
-
     }
 
     public void setLocale(Locale lang)
